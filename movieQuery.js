fc3794050f81ac84c8e8bd6ed50ae402dde1a339
Mahmuda AKter <mahmuda9326@gmail.com>
2017-11-17 20:16:55 +0600

    Assignment_5(updated)

--- movieQuery.js
+++ movieQuery.js
@@ -4,38 +4,41 @@ var movieList = [
   { title: "Omen : The Final Conflict", year: "1981", length: 108, rating: "6.7", genre: "horror" },
   { title: "Gladiator", year: "2000", length: 155, rating: "8.5", genre: "historical" },
   { title: "The Patriot", year: "2000", length: 165, rating: "7.1", genre: "historical" },
-  { title: "The Matrix Reloaded", year: "2003", length: 138, rating: "7.2", genre: "sci-fi" }
-];
+  { title: "The Matrix Reloaded”, year: “2003", length: 138, rating: "7.2", genre: "sci-fi" },
+  { title: "Interstellar”, year: “2014", length: 169, rating: "8.6", genre: "sci-fi" }
+  ];
 
 function getMovieTitle( index ){
-console.log(movieList[index].title);
+  return movieList[index].title;
 }
-console.log("The title of the movie:");
+
 console.log(getMovieTitle(3));
+console.log(getMovieTitle(0));
 
 function addMovie( movie ){
   movieList.push(movie);
-  console.log(movieList);
+  return movieList;
+
 }
 
-var newMovie = {title: "The Avengers", year: "2012", length: 143, rating: "8.1", genre: "sci-fi" };
+var newMovie = {title: "The Avangers", year: "2012", length: 143, rating: "8.1", genre: "sci-fi"};
 console.log(addMovie(newMovie));
 
-function movieByRating(){
-  movieList.sort(function(a, b){return b.rating - a.rating});
-  console.log(movieList);;
-}
 
+
+function movieByRating(){
+  movieList.sort(function(x, y){
+    return (y.rating)-(x.rating);
+  });
+  return movieList;
+}
 console.log(movieByRating());
 
 function findByTitle( title ){
-  for(var i=0; i<movieList.length; i++)
-  {
-    var temp = movieList[i].title.search(/avengers/i);
-    if(temp > 0)
-    {
-      console.log(movieList[i]);
-    }
-  }
-console.log(findByTitle("avengers"));
+  var list = [];
+  movieList.map(function(v,i){
+    if(v.title.toLowerCase().includes(title)) list.push(v);
+  });
+  return list;
+}
 console.log(findByTitle("matrix"));
